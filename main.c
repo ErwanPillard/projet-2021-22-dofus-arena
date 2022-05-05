fe681d156fd579cf0484717e51aa8047ef280a65
Erwan Pillard <erwan.pillard@edu.ece.fr>
2022-05-05 16:43:33 +0200

    rajout de la classe

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ int main(){
     al_register_event_source(queue, al_get_mouse_event_source());
 
     while(!end){
-        interface0(event, queue);
+        interface0();
         end = true;
         //dessinerInterface1();
         //dessinerInterfaceClasses();
@@ -63,7 +63,7 @@ int main(){
     }
 
 }
-
+/*
 int zebi(){
     //Déclarations
     ALLEGRO_DISPLAY* display = NULL;
@@ -178,28 +178,26 @@ int zebi(){
 
     al_play_sample_instance(musicInstance);
     //Boucle d événements
-    while(!end){
+    while(!end) {
         al_wait_for_event(queue, &event);
         switch (event.type) {
-            case ALLEGRO_EVENT_MOUSE_AXES:{
+            case ALLEGRO_EVENT_MOUSE_AXES: {
                 switch (historiquePageActive[tailleLogique]) {
-                    case INTERFACE0:{
-                        for (int i = 0; i < 3 ;i++) {
-                            if(surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[i])){
+                    case INTERFACE0: {
+                        for (int i = 0; i < 3; i++) {
+                            if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[i])) {
                                 rectangleAccueil[i].color = rectangleAccueil[i].colorThickness;
-                            }
-                            else{
+                            } else {
                                 rectangleAccueil[i].color = OR;
                             }
                         }
                         break;
                     }
-                    case INTERFACE1:{
+                    case INTERFACE1: {
                         for (int i = 0; i < 5; i++) {
-                            if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[i])){
+                            if (surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[i])) {
                                 rectangleInterfaceChoixJoueurs[i].color = rectangleInterfaceChoixJoueurs[i].colorThickness;
-                            }
-                            else{
+                            } else {
                                 rectangleInterfaceChoixJoueurs[i].color = OR;
                             }
                         }
@@ -208,183 +206,180 @@ int zebi(){
                 }
                 break;
             }
-            case ALLEGRO_EVENT_MOUSE_BUTTON_UP:{
+            case ALLEGRO_EVENT_MOUSE_BUTTON_UP: {
                 switch (historiquePageActive[tailleLogique]) {
-                    case INTERFACE0:{
+                    case INTERFACE0: {
                         for (int i = 0; i < 3; i++) {
-                            if(surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[0])){
+                            if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[0])) {
                                 al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
                                 tailleLogique++;
                                 historiquePageActive[tailleLogique] = INTERFACE1;
-                            }
-                            else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[1])){
+                            } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[1])) {
                                 //charger un ancienne partie
-                            }
-                            else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[2])){
+                            } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[2])) {
                                 end = true;
                             }
                         }
                         break;
                     }
-                    case INTERFACE1:{
-                        if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[0])){
+                    case INTERFACE1: {
+                        if (surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[0])) {
                             al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
                             tailleLogique++;
                             historiquePageActive[tailleLogique] = INTERFACE2;
                             donneePartie.nbJoueurs = 2;
-                        }
-                        else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[1])){
+                        } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[1])) {
                             al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
                             tailleLogique++;
                             historiquePageActive[tailleLogique] = INTERFACE2;
                             donneePartie.nbJoueurs = 3;
-                        }
-                        else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[2])){
+                        } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[2])) {
                             al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
                             tailleLogique++;
                             historiquePageActive[tailleLogique] = INTERFACE2;
                             donneePartie.nbJoueurs = 4;
-                        }
-                        else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[3])){
+                        } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[3])) {
                             al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
                             tailleLogique++;
                             historiquePageActive[tailleLogique] = INTERFACE2;
                             donneePartie.nbJoueurs = 1;
                         }
                         break;
-                for (int i = 0; i < 3; i++) {
-                    if(historiquePageActive[tailleLogique] == INTERFACE0){
-                        if(surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[0])){
-                            al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
-                            tailleLogique++;
-                            historiquePageActive[tailleLogique] = INTERFACE1;
+                        for (int i = 0; i < 3; i++) {
+                            if (historiquePageActive[tailleLogique] == INTERFACE0) {
+                                if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[0])) {
+                                    al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
+                                    tailleLogique++;
+                                    historiquePageActive[tailleLogique] = INTERFACE1;
+                                } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[1])) {
+                                    //charger un ancienne partie
+                                } else if (surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[2])) {
+                                    end = true;
+                                }
+                            }
                         }
-                        else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[1])){
-                            //charger un ancienne partie
+                        if (historiquePageActive[tailleLogique] == INTERFACE1) {
+                            if (surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[0])) {
+                                al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
+                                tailleLogique++;
+                                historiquePageActive[tailleLogique] = INTERFACE2;
+                                //nbJoueurs = 2;
+                            } else if (surPassageCase(event.mouse.x, event.mouse.y,
+                                                      rectangleInterfaceChoixJoueurs[1])) {
+                                al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
+                                tailleLogique++;
+                                historiquePageActive[tailleLogique] = INTERFACE2;
+                                //nbJoueurs = 2;
+                            } else if (surPassageCase(event.mouse.x, event.mouse.y,
+                                                      rectangleInterfaceChoixJoueurs[2])) {
+                                al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
+                                tailleLogique++;
+                                historiquePageActive[tailleLogique] = INTERFACE2;
+                                //nbJoueurs = 4;
+                            } else if (surPassageCase(event.mouse.x, event.mouse.y,
+                                                      rectangleInterfaceChoixJoueurs[3])) {
+                                al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
+                                tailleLogique++;
+                                historiquePageActive[tailleLogique] = INTERFACE2;
+                                //nbJoueurs = 1;
+                            }
                         }
-                        else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleAccueil[2])){
-                            end = true;
-                        }
-                    }
-                }
-                if(historiquePageActive[tailleLogique] == INTERFACE1){
-                    if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[0])){
-                        al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
-                        tailleLogique++;
-                        historiquePageActive[tailleLogique] = INTERFACE2;
-                        nbJoueurs = 2;
-                    }
-                    else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[1])){
-                        al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
-                        tailleLogique++;
-                        historiquePageActive[tailleLogique] = INTERFACE2;
-                        nbJoueurs = 2;
-                    }
-                    else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[2])){
-                        al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
-                        tailleLogique++;
-                        historiquePageActive[tailleLogique] = INTERFACE2;
-                        nbJoueurs = 4;
-                    }
-                    else if(surPassageCase(event.mouse.x, event.mouse.y, rectangleInterfaceChoixJoueurs[3])){
-                        al_play_sample(whoosh, 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_ONCE, 0);
-                        tailleLogique++;
-                        historiquePageActive[tailleLogique] = INTERFACE2;
-                        nbJoueurs = 1;
-                    }
-                }
 
-                if(historiquePageActive[tailleLogique] == ECHAP && clicExterneRectangle(event.mouse.x, event.mouse.y, rectangleMenuEchap)){
-                    tailleLogique++;
-                    historiquePageActive[tailleLogique] = historiquePageActive[tailleLogique - 2];
-                    case ECHAP:{
-                        if(clicExterneRectangle(event.mouse.x, event.mouse.y, rectangleMenuEchap)){
+                        if (historiquePageActive[tailleLogique] == ECHAP &&
+                            clicExterneRectangle(event.mouse.x, event.mouse.y, rectangleMenuEchap)) {
                             tailleLogique++;
                             historiquePageActive[tailleLogique] = historiquePageActive[tailleLogique - 2];
+                            case ECHAP: {
+                                if (clicExterneRectangle(event.mouse.x, event.mouse.y, rectangleMenuEchap)) {
+                                    tailleLogique++;
+                                    historiquePageActive[tailleLogique] = historiquePageActive[tailleLogique - 2];
+                                }
+                            }
                         }
-                    }
-                }
 
-                break;
-            }
-            case ALLEGRO_EVENT_KEY_DOWN:{
-                switch (event.keyboard.keycode) {
-                    case ALLEGRO_KEY_ESCAPE:{
-                        if (historiquePageActive[tailleLogique] != ECHAP) {
-                            tailleLogique++;
-                            historiquePageActive[tailleLogique] = ECHAP;
+                        break;
+                    }
+                    case ALLEGRO_EVENT_KEY_DOWN: {
+                        switch (event.keyboard.keycode) {
+                            case ALLEGRO_KEY_ESCAPE: {
+                                if (historiquePageActive[tailleLogique] != ECHAP) {
+                                    tailleLogique++;
+                                    historiquePageActive[tailleLogique] = ECHAP;
+                                }
+                                break;
+                            }
                         }
                         break;
                     }
-                }
-                break;
-            }
-            case ALLEGRO_EVENT_DISPLAY_CLOSE:{
-                end = true;
-                break;
-            }
-        }
-
-        switch(historiquePageActive[tailleLogique]){
-            case INTERFACE0:{
-                dessinerInterface0(imagePrincipale, rectangleAccueil);
-                break;
-            }
-            case INTERFACE1:{
-                dessinerInterface1(imagePrincipale, nbJoueurs1, nbJoueurs2, nbJoueurs3, nbJoueurs4 ,rectangleInterfaceChoixJoueurs);
-                break;
-            }
-            case INTERFACE2:{
-                int classeEnCours = 0; // lorsque qu'on arrive sur l'interface ou a une premiere classe par default a l'ecran
-                int posJoueur = 0;
-
-                for (int i = 0; i < donneePartie.nbJoueurs; i++) {
-                    while(joueur[i].classe == 5) {
-                        dessinerInterfaceClasses(classes, classeEnCours, posJoueur);
-                        classe(event, queue, joueur, classes, i, classeEnCours, posJoueur);
+                    case ALLEGRO_EVENT_DISPLAY_CLOSE: {
+                        end = true;
+                        break;
                     }
                 }
-                break;
+
+                switch (historiquePageActive[tailleLogique]) {
+                    case INTERFACE0: {
+                        dessinerInterface0(imagePrincipale, rectangleAccueil);
+                        break;
+                    }
+                    case INTERFACE1: {
+                        dessinerInterface1(imagePrincipale, nbJoueurs1, nbJoueurs2, nbJoueurs3, nbJoueurs4,
+                                           rectangleInterfaceChoixJoueurs);
+                        break;
+                    }
+                    case INTERFACE2: {
+                        int classeEnCours = 0; // lorsque qu'on arrive sur l'interface ou a une premiere classe par default a l'ecran
+                        int posJoueur = 0;
+
+                        for (int i = 0; i < donneePartie.nbJoueurs; i++) {
+                            while (joueur[i].classe == 5) {
+                                dessinerInterfaceClasses(classes, classeEnCours, posJoueur);
+                                classe(event, queue, joueur, classes, i, classeEnCours, posJoueur);
+                            }
+                        }
+                        break;
+                    }
+                    case ARENE: {
+                        al_clear_to_color(NOIR);
+                        al_draw_bitmap(map1, 0, 0, 0);
+                        al_flip_display();
+                        break;
+                    }
+                    case ECHAP: {
+                        dessierMenuEchap(rectangleMenuEchap);
+                        break;
+                    }
+                    case CHARGEMENT: {
+                    }
+                }
             }
-            case ARENE:{
-                al_clear_to_color(NOIR);
-                al_draw_bitmap(map1, 0, 0, 0);
-                al_flip_display();
-                break;
-            }
-            case ECHAP:{
-                dessierMenuEchap(rectangleMenuEchap);
-                break;
-            }
-            case CHARGEMENT:{
-            }
+
+                //Libération de la mémoire
+                al_destroy_display(display);
+
+                //Bitmap
+                al_destroy_bitmap(imagePrincipale);
+                al_destroy_bitmap(classes[0].fondEcran);
+
+                //Font
+                al_destroy_font(rectangleAccueil[0].font);
+                al_destroy_font(rectangleAccueil[1].font);
+                al_destroy_font(rectangleAccueil[2].font);
+
+                al_destroy_font(rectangleInterfaceChoixJoueurs[0].font);
+                al_destroy_font(rectangleInterfaceChoixJoueurs[1].font);
+                al_destroy_font(rectangleInterfaceChoixJoueurs[2].font);
+                al_destroy_font(rectangleInterfaceChoixJoueurs[3].font);
+
+                //Sample
+                al_destroy_sample(music_Accueil);
+                al_destroy_sample_instance(musicInstance);
+                al_destroy_sample(whoosh);
+
+                //Event
+                al_destroy_event_queue(queue);
+                return 0;
+
+
         }
-    }
-
-    //Libération de la mémoire
-    al_destroy_display(display);
-
-    //Bitmap
-    al_destroy_bitmap(imagePrincipale);
-    al_destroy_bitmap(classes[0].fondEcran);
-
-    //Font
-    al_destroy_font(rectangleAccueil[0].font);
-    al_destroy_font(rectangleAccueil[1].font);
-    al_destroy_font(rectangleAccueil[2].font);
-
-    al_destroy_font(rectangleInterfaceChoixJoueurs[0].font);
-    al_destroy_font(rectangleInterfaceChoixJoueurs[1].font);
-    al_destroy_font(rectangleInterfaceChoixJoueurs[2].font);
-    al_destroy_font(rectangleInterfaceChoixJoueurs[3].font);
-
-    //Sample
-    al_destroy_sample(music_Accueil);
-    al_destroy_sample_instance(musicInstance);
-    al_destroy_sample(whoosh);
-
-    //Event
-    al_destroy_event_queue(queue);
-    return 0;
-}
-
+        */
