7d598ad5097c3a5d0cecb856ebf5ff04c9bf5480
Yannis <yannis.soliveres@edu.ece.fr>
2022-05-11 15:52:00 +0200

    sauvegarde mon gava

--- Sorts.h
+++ Sorts.h
@@ -5,6 +5,7 @@
 typedef struct{
     int portéeX;
     int porteeY;
+    int nomSort;
 }Sort;
 
 #endif //ING1_PROJET_2021_22_DOFUS_ARENA_TEAM_AA_SORTS_H
--- sauvegarde.h
+++ sauvegarde.h
@@ -6,13 +6,19 @@
 #define ING1_PROJET_2021_22_DOFUS_ARENA_TEAM_AA_SAUVEGARDE_H
 #include "joueur.h"
 #include "partie.h"
+#include "classe.h"
 #include <wchar.h>
 #include <stdio.h>
+#include <string.h>
 #define CHEMIN_SAUVEGARDE "C:/Users"
 #define LG_MAX_CHEMIN_SAUVEGARDE 128
 #define LG_MAX_NOM_PARTIE 64
 
 FILE* creerFichier();
+void copierJoueur(Joueur j[], Partie donneePartie, FILE* creerFichier(), FILE *myFile, Classe classe);
+int copierMAPDansFichier(FILE *myFile);
+int fermerFichier(FILE* myFile);
+int sauvegarderPartie();
 
 
 #endif //ING1_PROJET_2021_22_DOFUS_ARENA_TEAM_AA_SAUVEGARDE_H
