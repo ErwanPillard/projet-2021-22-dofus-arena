a25b9c0a2605982b4611cf78dcfacb9682253f2f
yannis <yannis.soliveres@edu.ece.fr>
2022-06-02 20:29:21 +0200

    menu echap

--- echap.c
+++ echap.c
@@ -6,36 +6,36 @@
 #define NB_RECT 2 // 2 rectangle
 
 Rect initialisationMenuEchap(Rect* r){
-    r[0].largeur = 300;
-    r[0].hauteur = 250;
+    r[0].largeur = 400;
+    r[0].hauteur = 350;
     r[0].x = (float)LARGEUR / 2 - r[0].largeur / 2;
     r[0].y = (float)HAUTEUR / 2 - r[0].hauteur / 2;
-    r[0].color = NOIR;
+    r[0].color = OR;
     r[0].rounded_x = 10;
     r[0].rounded_y = 10;
     r[0].thickness = 5;
-    r[0].font = al_load_font("../Polices/Achafont.ttf", 20, 0);
+    r[0].font = al_load_font("../Polices/Achafont.ttf", 30, 0);
 
-    r[1].largeur = 300;
-    r[1].hauteur = 250;
+    r[1].largeur = 400;
+    r[1].hauteur = 350;
     r[1].x = (float)LARGEUR / 2 - r[0].largeur / 2;
     r[1].y = (float)HAUTEUR / 2 - r[0].hauteur / 2;
-    r[1].color = NOIR;
+    r[1].color = OR;
     r[1].rounded_x = 10;
     r[1].rounded_y = 10;
     r[1].thickness = 5;
-    r[1].font = al_load_font("../Polices/Achafont.ttf", 20, 0);
+    r[1].font = al_load_font("../Polices/Achafont.ttf", 30, 0);
 }
 
 void dessinerMenuEchap(Rect r[], ALLEGRO_BITMAP* imagePrincipale){ // Menu doit aussi être accessible a la souris
 
     al_draw_bitmap(imagePrincipale, 0, 0, 0);
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 3; i++) {
         al_draw_filled_rectangle(r[i].x, r[i].y, r[i].x + r[i].largeur, r[i].y + r[i].hauteur, r[i].color);
         al_draw_rectangle(r[i].x, r[i].y, r[i].x + r[i].largeur, r[i].y + r[i].hauteur, r[i].colorThickness, r[i].thickness);
     }
-    al_draw_text(r[0].font, NOIR, r[0].x + 70, r[0].y + 6, 0, "SAUVEGARDER");
-    al_draw_text(r[1].font, NOIR, r[1].x + 50, r[1].y + 6, 0, "DIDACTICIEL");
+    al_draw_text(r[0].font, NOIR, r[0].x + 125, r[0].y + 50, 0, "SAUVEGARDER");
+    al_draw_text(r[1].font, NOIR, r[1].x + 125, r[1].y + 250, 0, "DIDACTICIEL");
 
     al_flip_display();
 }
