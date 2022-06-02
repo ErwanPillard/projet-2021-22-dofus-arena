9b0c0b6f7552e4bc7a02e05fe12a383059d37e84
erwanpillard <erwan.pillard@edu.ece.fr>
2022-06-02 19:52:26 +0200

    sort + animation sorts

--- Sorts.h
+++ Sorts.h
@@ -48,7 +48,6 @@ typedef struct {
 
 void bouleDeFeux();
 void traineeFeux();
-void dessinerSortsGlace(ALLEGRO_EVENT_QUEUE *queue, ALLEGRO_EVENT event);
 void dessinerSorts2(ALLEGRO_EVENT_QUEUE *queue, ALLEGRO_EVENT event);
 
 
--- interfaceSort.h
+++ interfaceSort.h
@@ -10,6 +10,7 @@
 #include "arene.h"
 #include "partie.h"
 
+
 void choixSort(ALLEGRO_EVENT event, ALLEGRO_EVENT_QUEUE *queue, CoordonneeISO coordonneeIso[][12], Joueur joueur[], Classe classe[],Partie donneePartie, ALLEGRO_TIMER *timer, Rect r[5], int map[][12], int numSort, int type);
 
 void dessinerSortsGlace(ALLEGRO_EVENT_QUEUE *queue, ALLEGRO_EVENT event, CoordonneeISO coordonneeIso[][12], Joueur joueur[], Classe classe[], Partie *donneePartie, Rect r[5], int cibleX, int cibleY);
